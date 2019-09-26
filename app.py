241d1fbd521bd6ad3823bb3763177dc70f61bb9d
Justin <showin@live.com>
2019-09-26 12:07:19 -0700

    update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import numpy as np
+import pandas as pd
 import pickle
 from gensim.utils import simple_preprocess
 from gensim.parsing.preprocessing import STOPWORDS
@@ -17,51 +18,79 @@ def tokenize(text):
     return [token for token in simple_preprocess(text) if token not in STOPWORDS]
 
 
+import requests
 @app.route('/', methods=['POST', 'GET'])
+def responses():
+    response = requests.get('https://bnb-web-backend.herokuapp.com/api/features')
+    return str(response.text)
+
+data = {"feature":[{"id":3,"neighbourhood_group_cleansed":"Spandau","description":"its is a description","property_type":"apartment","accommodates":2,"bathrooms":1,"security_deposit":200,"cleaning_fee":3,"guests_included":0,"extra_people":0,"minimum_nights":1,"instant_bookable":False,"cancellation_policy":"flexible","tv_cable":False,"pets_allowed":False,"bedrooms":1},{"id":2,"neighbourhood_group_cleansed":"maybe","description":"its is a description","property_type":"huge","accommodates":0,"bathrooms":1,"security_deposit":200,"cleaning_fee":3,"guests_included":0,"extra_people":0,"minimum_nights":1,"instant_bookable":False,"cancellation_policy":"big money","tv_cable":True,"pets_allowed":False,"bedrooms":7}]}
+
+@app.route('/api', methods=['POST', 'GET'])
 def predict():
     """
     Receives JSON as input and outputs optimal price
     """
-
+    #response = requests.get('https://bnb-web-backend.herokuapp.com/api/features')
+    
     # retrieve json user input data
-    data = request.get_json(force=True)
+    #data = response.json()
 
     # Assign incoming data to variables for use in model
-    desc = req_data['description'] 
+    desc = data['feature'][0]['description'] 
 
     # Run description through tokenize function
     description_length = len(tokenize(desc))
 
     # Continue assigning incoming data to variables for use in model
-    neighbourhood_group_cleansed = req_data['neighbourhood_group_cleansed']
-    property_type = req_data['property_type']
-    accommodates = req_data['accommodates']
-    bathrooms = req_data['bathrooms'] 
-    bedrooms = req_data['bedrooms'] 
-    security_deposit = req_data['security_deposit'] 
-    cleaning_fee = req_data['cleaning_fee']                   
-    guests_included = req_data['guests_included']                
-    extra_people = req_data['extra_people']                   
-    minimum_nights = req_data['minimum_nights']                   
-    instant_bookable = req_data['instant_bookable']                 
-    cancellation_policy = req_data['cancellation_policy']          
-    tv_cable = req_data['tv_cable']                             
-    pets_allowed = req_data['pets_allowed']                      
+    neighbourhood_group_cleansed = data['feature'][0]['neighbourhood_group_cleansed']
+    property_type = data['feature'][0]['property_type']
+    accommodates = data['feature'][0]['accommodates']
+    bathrooms = data['feature'][0]['bathrooms'] 
+    bedrooms = data['feature'][0]['bedrooms'] 
+    security_deposit = data['feature'][0]['security_deposit'] 
+    cleaning_fee = data['feature'][0]['cleaning_fee']                   
+    guests_included = data['feature'][0]['guests_included']                
+    extra_people = data['feature'][0]['extra_people']                   
+    minimum_nights = data['feature'][0]['minimum_nights']                   
+    instant_bookable = data['feature'][0]['instant_bookable']                 
+    cancellation_policy = data['feature'][0]['cancellation_policy']          
+    tv_cable = data['feature'][0]['tv_cable']                             
+    pets_allowed = data['feature'][0]['pets_allowed']                      
 
     # convert to list for model
-    features = [neighbourhood_group_cleansed, property_type, accommodates,
-                bathrooms, bedrooms, security_deposit, cleaning_fee, guests_included,
-                extra_people, minimum_nights, instant_bookable, cancellation_policy, 
-                description_length, tv_cable, pets_allowed]
     
+    feature_dict = {'neighbourhood_group_cleansed':neighbourhood_group_cleansed, 
+                    'property_type':property_type, 
+                    'accommodates':accommodates,
+                    'bathrooms':bathrooms, 
+                    'bedrooms':bedrooms, 
+                    'security_deposit':security_deposit, 
+                    'cleaning_fee':cleaning_fee,
+                    'guests_included':guests_included, 
+                    'extra_people':extra_people, 
+                    'minimum_nights':minimum_nights, 
+                    'instant_bookable':instant_bookable,
+                    'cancellation_policy':cancellation_policy, 
+                    'description_length':description_length, 
+                    'tv':tv_cable, 
+                    'pets':pets_allowed}
+
+    features = pd.DataFrame(feature_dict, index=[1])
+    
+    encoded_features = np.array([neighbourhood_group_cleansed, property_type,
+                        instant_bookable, cancellation_policy,
+                        tv_cable, pets_allowed, bathrooms]).reshape(1, -1)
+    print(features)
     # run the features through the encoder
     features_transformed = encoder.transform(features)
-
+   # print(features_transformed)
+    
     # predict optimal price using the prediction function
     price = model.predict(features_transformed)
-
+    print(price.shape)
     # return the optimal price in json format
-    return  jsonify({'prediction': price})
+    return jsonify({'prediction': price[0]})
 
 
 
